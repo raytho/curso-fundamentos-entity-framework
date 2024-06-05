a465efcda60894bd680e99497d2c29b0be540ae8
Raymundo Solis <pepperray01@gmail.com>
2024-06-04 18:44:28 -0600

    EF Course | Data Anotations

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace proyectoef.Models;
 
 public class Tarea
 {
+    [Key]
     public Guid TareaId {get;set;}
+    [ForeignKey("CategoriaId")]
     public Guid CategoriaId {get;set;}
 
+    [Required]
+    [MaxLength(200)]
     public string Titulo {get;set;}
 
     public string Descripcion {get;set;}
@@ -14,6 +21,9 @@ public class Tarea
     public DateTime FechaCreacion {get;set;}
     
     public virtual Categoria Categoria {get;set;}
+
+    [NotMapped]
+    public string Resumen { get; set; }
 }
 
 public enum Prioridad
